37e2005722ed56811934f8acc2824ca7c72ec565
Kyla Wootson <kylawootson@MacBook-Pro.local>
2022-04-15 13:58:33 -0400

    Saved Channges to JS file

--- classChallenge.js
+++ classChallenge.js
@@ -4,7 +4,9 @@ if (userInput == 1){
     console.log("How much would you like to deposit?");
     let userInput2 = prompt("How much would you like to deposit?");
 }
-alert('Your new balance is: [THE BALANCE]')
+alert('Your new balance is: [THE BALANCE]');
 if (userInput == 2){
-    console.log("How much would you like to withdraw?")
+    console.log("How much would you like to withdraw?");
+    let userInput3 = prompt("How much would you like to withdraw?");
 }
+alert("Your new balance is: [THE BALANCE]")
