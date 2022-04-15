3716e373a002cd177af39179509f4d9cd65c05c2
Kyla Wootson <kylawootson@MacBook-Pro.local>
2022-04-15 13:55:54 -0400

    Added JS file

---
+++ classChallenge.js
@@ -0,0 +1,10 @@
+let userInput = prompt("Please choose either number 1. Deposit Money or 2. Withdraw Money");
+
+if (userInput == 1){
+    console.log("How much would you like to deposit?");
+    let userInput2 = prompt("How much would you like to deposit?");
+}
+alert('Your new balance is: [THE BALANCE]')
+if (userInput == 2){
+    console.log("How much would you like to withdraw?")
+}
